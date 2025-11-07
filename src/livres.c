7871c02902256a8aa4f8725b490b403976de6104
mariievrcl <49845362+mariievrcl@users.noreply.github.com>
2025-11-07 09:05:38 +0100

    livres.c + livres.h

--- src/livres.c
+++ src/livres.c
@@ -1,3 +1,32 @@
 //
 // Created by Marie Viricel on 06/11/2025.
 //
+#include <stdio.h>
+#include <string.h>
+#include "livres.h"
+
+void ajouterLivre(Livre *livres, int *nbLivres) {
+    printf("[TODO] ajouterLivre\n");
+    (void)livres; (void)nbLivres;
+}
+void supprimerLivre(Livre *livres, int *nbLivres, int id) {
+    printf("[TODO] supprimerLivre id=%d\n", id);
+    (void)livres; (void)nbLivres;
+}
+void modifierLivre(Livre *livres, int nbLivres, int id) {
+    printf("[TODO] modifierLivre id=%d\n", id);
+    (void)livres; (void)nbLivres;
+}
+void afficherLivres(Livre *livres, int nbLivres) {
+    printf("[TODO] afficherLivres (%d livres)\n", nbLivres);
+    (void)livres;
+}
+int rechercherLivre(Livre *livres, int nbLivres, char *titre) {
+    printf("[TODO] rechercherLivre titre=\"%s\"\n", titre ? titre : "(null)");
+    (void)livres; (void)nbLivres;
+    return -1;
+}
+void trierLivres(Livre *livres, int nbLivres) {
+    printf("[TODO] trierLivres (%d)\n", nbLivres);
+    (void)livres;
+}
