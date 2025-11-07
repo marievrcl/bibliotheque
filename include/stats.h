ee47563b6dd7777d4575160000d4727667c7f903
mariievrcl <49845362+mariievrcl@users.noreply.github.com>
2025-11-07 21:10:28 +0100

    création stats.h

--- include/stats.h
+++ include/stats.h
@@ -2,20 +2,25 @@
 // Created by Marie Viricel on 06/11/2025.
 //
 
-#ifndef BIBLIOTHEQUE_STATS_H
-#define BIBLIOTHEQUE_STATS_H
-
-#endif //BIBLIOTHEQUE_STATS_H
-
 #ifndef STATS_H
 #define STATS_H
+
 #include "livres.h"
 #include "utilisateurs.h"
 #include "emprunts.h"
 
-void afficherStatistiques(Emprunt *emprunts, int nbEmprunts, Utilisateur *users, int nbUsers, Livre *livres, int nbLivres);
-void livresLesPlusEmpruntes(Emprunt *emprunts, int nbEmprunts);
-void utilisateurLePlusActif(Emprunt *emprunts, Utilisateur *users, int nbUsers);
+// Fonction principale : affichage global des stats
+void afficherStatistiques(Emprunt *emprunts, int nbEmprunts,
+                          Utilisateur *users, int nbUsers,
+                          Livre *livres, int nbLivres);
+
+// Sous-fonctions utilisées par afficherStatistiques
+void livresLesPlusEmpruntes(Emprunt *emprunts, int nbEmprunts,
+                            Livre *livres, int nbLivres);
+
+void utilisateurLePlusActif(Emprunt *emprunts, int nbEmprunts,
+                            Utilisateur *users, int nbUsers);
 
 #endif
+
 
