a8d3a1abda8f5fb290352fe1f6c130e5bd597a51
mariievrcl <49845362+mariievrcl@users.noreply.github.com>
2025-11-07 09:01:55 +0100

    création structure + emprunt.c

--- include/utilisateurs.h
+++ include/utilisateurs.h
@@ -1,21 +1,3 @@
 //
 // Created by Marie Viricel on 06/11/2025.
 //
-
-#ifndef UTILISATEURS_H
-#define UTILISATEURS_H
-
-typedef struct {
-    int id;
-    char nom[50];
-    char prenom[50];
-    char email[100];
-    int quota; // nb de livres empruntés
-} Utilisateur;
-
-void ajouterUtilisateur(Utilisateur *users, int *nbUsers);
-void supprimerUtilisateur(Utilisateur *users, int *nbUsers, int id);
-void afficherUtilisateurs(Utilisateur *users, int nbUsers);
-int  authentifierUtilisateur(char *email);
-
-#endif
--- src/utilisateurs.c
+++ src/utilisateurs.c
@@ -2,23 +2,3 @@
 // Created by Marie Viricel on 06/11/2025.
 //
 
-#include <stdio.h>
-#include <string.h>
-#include "utilisateurs.h"
-
-void ajouterUtilisateur(Utilisateur *users, int *nbUsers) {
-    printf("[TODO] ajouterUtilisateur\n");
-    (void)users; (void)nbUsers;
-}
-void supprimerUtilisateur(Utilisateur *users, int *nbUsers, int id) {
-    printf("[TODO] supprimerUtilisateur id=%d\n", id);
-    (void)users; (void)nbUsers;
-}
-void afficherUtilisateurs(Utilisateur *users, int nbUsers) {
-    printf("[TODO] afficherUtilisateurs (%d)\n", nbUsers);
-    (void)users;
-}
-int authentifierUtilisateur(char *email) {
-    printf("[TODO] authentifierUtilisateur email=%s\n", email ? email : "(null)");
-    return 1; // par défaut : "ok"
-}
