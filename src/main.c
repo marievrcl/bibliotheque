c8255df1d521e3fdd5578f64b450a86105e1a0a7
mariievrcl <49845362+mariievrcl@users.noreply.github.com>
2025-11-07 09:18:22 +0100

    ajout gestion des livres (afficher/ ajouter livres ou retour)

--- src/main.c
+++ src/main.c
@@ -6,6 +6,10 @@
 #include "livres.h"
 #include "utilisateurs.h"
 #include "emprunts.h"
+#include <stdio.h>
+#include "livres.h"
+#include "utilisateurs.h"
+#include "emprunts.h"
 #include "fichiers.h"
 #include "stats.h"
 
@@ -15,6 +19,7 @@ int main(void) {
     Emprunt emprunts[100];
     int nbLivres = 0, nbUsers = 0, nbEmprunts = 0;
 
+    // Chargement des données depuis les fichiers
     chargerLivres(livres, &nbLivres);
     chargerUtilisateurs(utilisateurs, &nbUsers);
     chargerEmprunts(emprunts, &nbEmprunts);
@@ -31,17 +36,58 @@ int main(void) {
         if (scanf("%d", &choix) != 1) choix = 0;
 
         switch (choix) {
-            case 1: afficherLivres(livres, nbLivres); break;
-            case 2: afficherUtilisateurs(utilisateurs, nbUsers); break;
-            case 3: verifierRetards(emprunts, nbEmprunts); break;
-            case 4: afficherStatistiques(emprunts, nbEmprunts, utilisateurs, nbUsers, livres, nbLivres); break;
-            case 0: printf("Sauvegarde et fermeture...\n"); break;
-            default: printf("Choix invalide.\n");
+            case 1: {
+                int sousChoix;
+                do {
+                    printf("\n--- GESTION DES LIVRES ---\n");
+                    printf("1. Afficher les livres\n");
+                    printf("2. Ajouter un livre\n");
+                    printf("0. Retour\n");
+                    printf("Choix : ");
+                    if (scanf("%d", &sousChoix) != 1) sousChoix = 0;
+
+                    switch (sousChoix) {
+                        case 1:
+                            afficherLivres(livres, nbLivres);
+                            break;
+                        case 2:
+                            ajouterLivre(livres, &nbLivres);
+                            break;
+                        case 0:
+                            printf("Retour au menu principal...\n");
+                            break;
+                        default:
+                            printf("Choix invalide.\n");
+                    }
+                } while (sousChoix != 0);
+                break;
+            }
+
+            case 2:
+                afficherUtilisateurs(utilisateurs, nbUsers);
+                break;
+
+            case 3:
+                verifierRetards(emprunts, nbEmprunts);
+                break;
+
+            case 4:
+                afficherStatistiques(emprunts, nbEmprunts, utilisateurs, nbUsers, livres, nbLivres);
+                break;
+
+            case 0:
+                printf("Sauvegarde et fermeture...\n");
+                break;
+
+            default:
+                printf("Choix invalide.\n");
         }
     } while (choix != 0);
 
+    // Sauvegarde à la fermeture
     sauvegarderLivres(livres, nbLivres);
     sauvegarderUtilisateurs(utilisateurs, nbUsers);
     sauvegarderEmprunts(emprunts, nbEmprunts);
+
     return 0;
 }
