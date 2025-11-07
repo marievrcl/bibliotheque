38bc85921119f2ca4d914f5ef86095325011a09d
mariievrcl <49845362+mariievrcl@users.noreply.github.com>
2025-11-07 09:07:45 +0100

    stats.c + stats.h

--- include/stats.h
+++ include/stats.h
@@ -2,4 +2,20 @@
 // Created by Marie Viricel on 06/11/2025.
 //
 
+#ifndef BIBLIOTHEQUE_STATS_H
+#define BIBLIOTHEQUE_STATS_H
+
+#endif //BIBLIOTHEQUE_STATS_H
+
+#ifndef STATS_H
+#define STATS_H
+#include "livres.h"
+#include "utilisateurs.h"
+#include "emprunts.h"
+
+void afficherStatistiques(Emprunt *emprunts, int nbEmprunts, Utilisateur *users, int nbUsers, Livre *livres, int nbLivres);
+void livresLesPlusEmpruntes(Emprunt *emprunts, int nbEmprunts);
+void utilisateurLePlusActif(Emprunt *emprunts, Utilisateur *users, int nbUsers);
+
+#endif
 
