173907137bc0408ea2e13b5e77e1a2386125b953
mariievrcl <49845362+mariievrcl@users.noreply.github.com>
2025-12-03 14:08:25 +0100

    bouton marchent

--- src/main.c
+++ src/main.c
@@ -25,188 +25,225 @@
 #define MAX_USERS 100
 #define MAX_EMPRUNTS 100
 
-// ==================== Buffers et flags pour saisie ====================
+// =================== Buffers pour TextBox ===================
 char titreInput[100] = "", auteurInput[100] = "", categorieInput[50] = "", isbnInput[20] = "", anneeBuf[10] = "";
-int anneeInput = 0;
-bool titreEdit = false, auteurEdit = false, categorieEdit = false, isbnEdit = false, anneeEdit = false;
-
 char nomInput[50] = "", prenomInput[50] = "", emailInput[100] = "";
-bool nomEdit = false, prenomEdit = false, emailEdit = false;
+char livreASupprimerBuf[10] = "", userASupprimerBuf[10] = "";
 
-// ==================== Modes interface ====================
+// Flags TextBox actifs
+bool titreEdit=false, auteurEdit=false, categorieEdit=false, isbnEdit=false, anneeEdit=false;
+bool nomEdit=false, prenomEdit=false, emailEdit=false;
+
+// =================== Modes ===================
 typedef enum {
-    MENU,
-    LIVRES,
-    AJOUT_LIVRE,
-    SUPPR_LIVRE,
-    USERS,
-    AJOUT_USER,
-    SUPPR_USER,
-    EMPRUNTS,
-    STATS
+    MENU, LIVRES, AJOUT_LIVRE, SUPPR_LIVRE,
+    USERS, AJOUT_USER, SUPPR_USER,
+    EMPRUNTS, STATS
 } Mode;
 
 int main(void)
 {
-    // ========== Tableaux et compteurs ==========
+    // ======= Déclaration des données =======
     Livre livres[MAX_LIVRES];
     Utilisateur utilisateurs[MAX_USERS];
     Emprunt emprunts[MAX_EMPRUNTS];
-    int nbLivres = 0, nbUsers = 0, nbEmprunts = 0;
 
-    // ========== Chargement des fichiers ==========
+    int nbLivres=0, nbUsers=0, nbEmprunts=0;
+
+    // ======= Chargement depuis fichiers =======
     chargerLivres(livres, &nbLivres);
     chargerUtilisateurs(utilisateurs, &nbUsers);
     chargerEmprunts(emprunts, &nbEmprunts);
 
-    // ========== Fenêtre Raylib ==========
+    // ======= Initialisation fenêtre =======
     InitWindow(900, 600, "Bibliotheque - Interface Graphique");
     SetTargetFPS(60);
 
     Mode mode = MENU;
 
-    while (!WindowShouldClose())
+    while(!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        // ================= MENU PRINCIPAL =================
-        if (mode == MENU)
+        // ================== MENU PRINCIPAL ==================
+        if(mode==MENU)
         {
-            DrawText("MENU PRINCIPAL", 300, 30, 30, DARKGRAY);
-            if (GuiButton((Rectangle){300, 120, 300, 50}, "Gerer les livres")) mode = LIVRES;
-            if (GuiButton((Rectangle){300, 190, 300, 50}, "Gerer les utilisateurs")) mode = USERS;
-            if (GuiButton((Rectangle){300, 260, 300, 50}, "Gerer les emprunts")) mode = EMPRUNTS;
-            if (GuiButton((Rectangle){300, 330, 300, 50}, "Statistiques")) mode = STATS;
-            if (GuiButton((Rectangle){300, 400, 300, 50}, "Quitter")) break;
+            DrawText("MENU PRINCIPAL", 300,30,30,DARKGRAY);
+            if(GuiButton((Rectangle){300,120,300,50},"Gerer les livres")) mode=LIVRES;
+            if(GuiButton((Rectangle){300,190,300,50},"Gerer les utilisateurs")) mode=USERS;
+            if(GuiButton((Rectangle){300,260,300,50},"Gerer les emprunts")) mode=EMPRUNTS;
+            if(GuiButton((Rectangle){300,330,300,50},"Statistiques")) mode=STATS;
+            if(GuiButton((Rectangle){300,400,300,50},"Quitter")) break;
         }
 
-        // ===================== LIVRES =====================
-        else if (mode == LIVRES)
+        // ================== GESTION LIVRES ==================
+        else if(mode==LIVRES)
         {
-            DrawText("Liste des livres :", 50, 20, 25, DARKGRAY);
-            for (int i = 0; i < nbLivres; i++)
+            DrawText("Liste des livres :",50,20,25,DARKGRAY);
+            for(int i=0;i<nbLivres;i++)
             {
-                char buffer[200];
-                snprintf(buffer, sizeof(buffer), "%d - %s", livres[i].id, livres[i].titre);
-                DrawText(buffer, 50, 70 + i * 25, 20, BLACK);
+                char buf[200];
+                snprintf(buf,sizeof(buf),"%d - %s",livres[i].id,livres[i].titre);
+                DrawText(buf,50,70+i*25,20,BLACK);
             }
-
-            if (GuiButton((Rectangle){600, 70, 250, 40}, "Ajouter un livre")) mode = AJOUT_LIVRE;
-            if (GuiButton((Rectangle){600, 120, 250, 40}, "Supprimer un livre")) mode = SUPPR_LIVRE;
-            if (GuiButton((Rectangle){600, 500, 250, 40}, "Retour menu")) mode = MENU;
+            if(GuiButton((Rectangle){600,70,250,40},"Ajouter un livre")) mode=AJOUT_LIVRE;
+            if(GuiButton((Rectangle){600,120,250,40},"Supprimer un livre")) mode=SUPPR_LIVRE;
+            if(GuiButton((Rectangle){600,500,250,40},"Retour menu")) mode=MENU;
         }
 
-        // ===================== AJOUT LIVRE =====================
-        else if (mode == AJOUT_LIVRE)
+        // ================== AJOUT LIVRE ==================
+        else if(mode==AJOUT_LIVRE)
         {
-            DrawText("Ajouter un livre", 50, 30, 25, DARKGRAY);
+            DrawText("Ajouter un livre",50,30,25,DARKGRAY);
+            DrawText("Titre :",50,120,20,BLACK);
+            titreEdit = GuiTextBox((Rectangle){140,110,500,40},titreInput,sizeof(titreInput),titreEdit);
 
-            DrawText("Titre :", 50, 120, 20, BLACK);
-            if (GuiTextBox((Rectangle){140, 110, 500, 40}, titreInput, sizeof(titreInput), titreEdit)) titreEdit = !titreEdit;
+            DrawText("Auteur :",50,170,20,BLACK);
+            auteurEdit = GuiTextBox((Rectangle){140,160,500,40},auteurInput,sizeof(auteurInput),auteurEdit);
 
-            DrawText("Auteur :", 50, 170, 20, BLACK);
-            if (GuiTextBox((Rectangle){140, 160, 500, 40}, auteurInput, sizeof(auteurInput), auteurEdit)) auteurEdit = !auteurEdit;
+            DrawText("Catégorie :",50,220,20,BLACK);
+            categorieEdit = GuiTextBox((Rectangle){140,210,300,40},categorieInput,sizeof(categorieInput),categorieEdit);
 
-            DrawText("Categorie :", 50, 220, 20, BLACK);
-            if (GuiTextBox((Rectangle){140, 210, 300, 40}, categorieInput, sizeof(categorieInput), categorieEdit)) categorieEdit = !categorieEdit;
+            DrawText("ISBN :",50,270,20,BLACK);
+            isbnEdit = GuiTextBox((Rectangle){140,260,200,40},isbnInput,sizeof(isbnInput),isbnEdit);
 
-            DrawText("ISBN :", 50, 270, 20, BLACK);
-            if (GuiTextBox((Rectangle){140, 260, 200, 40}, isbnInput, sizeof(isbnInput), isbnEdit)) isbnEdit = !isbnEdit;
+            DrawText("Année :",50,320,20,BLACK);
+            anneeEdit = GuiTextBox((Rectangle){140,310,100,40},anneeBuf,sizeof(anneeBuf),anneeEdit);
 
-            DrawText("Annee :", 50, 320, 20, BLACK);
-            if (GuiTextBox((Rectangle){140, 310, 100, 40}, anneeBuf, sizeof(anneeBuf), anneeEdit)) anneeEdit = !anneeEdit;
-
-            if (GuiButton((Rectangle){140, 380, 200, 40}, "Valider"))
+            if(GuiButton((Rectangle){140,380,200,40},"Valider"))
             {
                 Livre nouveau;
-                strcpy(nouveau.titre, titreInput);
-                strcpy(nouveau.auteur, auteurInput);
-                strcpy(nouveau.categorie, categorieInput);
-                strcpy(nouveau.isbn, isbnInput);
+                strcpy(nouveau.titre,titreInput);
+                strcpy(nouveau.auteur,auteurInput);
+                strcpy(nouveau.categorie,categorieInput);
+                strcpy(nouveau.isbn,isbnInput);
                 nouveau.annee = atoi(anneeBuf);
-                nouveau.id = nbLivres + 1;
-                nouveau.disponible = 1;
+                ajouterLivreGui(livres,&nbLivres,nouveau);
 
-                ajouterLivreGui(livres, &nbLivres, nouveau);
+                titreInput[0]=auteurInput[0]=categorieInput[0]=isbnInput[0]='\0';
+                anneeBuf[0]='\0';
+                titreEdit=auteurEdit=categorieEdit=isbnEdit=anneeEdit=false;
 
-                titreInput[0] = auteurInput[0] = categorieInput[0] = isbnInput[0] = '\0';
-                anneeBuf[0] = '\0';
-
-                mode = LIVRES;
+                mode=LIVRES;
             }
 
-            if (GuiButton((Rectangle){140, 440, 200, 40}, "Annuler"))
+            if(GuiButton((Rectangle){140,440,200,40},"Annuler"))
             {
-                titreInput[0] = auteurInput[0] = categorieInput[0] = isbnInput[0] = '\0';
-                anneeBuf[0] = '\0';
-                mode = LIVRES;
+                titreInput[0]=auteurInput[0]=categorieInput[0]=isbnInput[0]='\0';
+                anneeBuf[0]='\0';
+                titreEdit=auteurEdit=categorieEdit=isbnEdit=anneeEdit=false;
+
+                mode=LIVRES;
             }
         }
 
-        // ===================== USERS =====================
-        else if (mode == USERS)
+        // ================== SUPPR LIVRE ==================
+        else if(mode==SUPPR_LIVRE)
         {
-            DrawText("Liste des utilisateurs :", 50, 20, 25, DARKGRAY);
-            for (int i = 0; i < nbUsers; i++)
+            DrawText("Supprimer un livre par ID :",50,100,25,DARKGRAY);
+            int idToDel = atoi(livreASupprimerBuf);
+            GuiTextBox((Rectangle){300,90,100,40},livreASupprimerBuf,sizeof(livreASupprimerBuf),true);
+            if(GuiButton((Rectangle){450,90,200,40},"Supprimer"))
             {
-                char buffer[200];
-                snprintf(buffer, sizeof(buffer), "%d - %s %s", utilisateurs[i].id, utilisateurs[i].nom, utilisateurs[i].prenom);
-                DrawText(buffer, 50, 70 + i * 25, 20, BLACK);
+                supprimerLivre(livres,&nbLivres,idToDel);
+                livreASupprimerBuf[0]='\0';
+                mode=LIVRES;
             }
-
-            if (GuiButton((Rectangle){600, 70, 250, 40}, "Ajouter un utilisateur")) mode = AJOUT_USER;
-            if (GuiButton((Rectangle){600, 120, 250, 40}, "Supprimer un utilisateur")) mode = SUPPR_USER;
-            if (GuiButton((Rectangle){600, 500, 250, 40}, "Retour menu")) mode = MENU;
+            if(GuiButton((Rectangle){600,500,250,40},"Retour")) mode=LIVRES;
         }
 
-        // ===================== AJOUT USER =====================
-        else if (mode == AJOUT_USER)
+        // ================== GESTION UTILISATEURS ==================
+        else if(mode==USERS)
         {
-            DrawText("Ajouter un utilisateur", 50, 30, 25, DARKGRAY);
-
-            DrawText("Nom :", 50, 120, 20, BLACK);
-            if (GuiTextBox((Rectangle){140, 110, 500, 40}, nomInput, sizeof(nomInput), nomEdit)) nomEdit = !nomEdit;
-
-            DrawText("Prenom :", 50, 170, 20, BLACK);
-            if (GuiTextBox((Rectangle){140, 160, 500, 40}, prenomInput, sizeof(prenomInput), prenomEdit)) prenomEdit = !prenomEdit;
-
-            DrawText("Email :", 50, 220, 20, BLACK);
-            if (GuiTextBox((Rectangle){140, 210, 500, 40}, emailInput, sizeof(emailInput), emailEdit)) emailEdit = !emailEdit;
-
-            if (GuiButton((Rectangle){140, 280, 200, 40}, "Valider"))
+            DrawText("Liste des utilisateurs :",50,20,25,DARKGRAY);
+            for(int i=0;i<nbUsers;i++)
             {
-                Utilisateur nouveau;
-                nouveau.id = nbUsers + 1;
-                strcpy(nouveau.nom, nomInput);
-                strcpy(nouveau.prenom, prenomInput);
-                strcpy(nouveau.email, emailInput);
-                nouveau.quota = 0;
+                char buf[200];
+                snprintf(buf,sizeof(buf),"%d - %s %s",utilisateurs[i].id,utilisateurs[i].nom,utilisateurs[i].prenom);
+                DrawText(buf,50,70+i*25,20,BLACK);
+            }
+            if(GuiButton((Rectangle){600,70,250,40},"Ajouter un utilisateur")) mode=AJOUT_USER;
+            if(GuiButton((Rectangle){600,120,250,40},"Supprimer un utilisateur")) mode=SUPPR_USER;
+            if(GuiButton((Rectangle){600,500,250,40},"Retour menu")) mode=MENU;
+        }
 
-                ajouterUtilisateurManuel(utilisateurs, &nbUsers, nouveau.nom, nouveau.prenom, nouveau.email, nouveau.quota);
+        // ================== AJOUT UTILISATEUR ==================
+        else if(mode==AJOUT_USER)
+        {
+            DrawText("Ajouter un utilisateur",50,30,25,DARKGRAY);
 
-                nomInput[0] = prenomInput[0] = emailInput[0] = '\0';
-                mode = USERS;
+            DrawText("Nom :",50,110,20,BLACK);
+            nomEdit = GuiTextBox((Rectangle){140,100,500,40},nomInput,sizeof(nomInput),nomEdit);
+
+            DrawText("Prénom :",50,160,20,BLACK);
+            prenomEdit = GuiTextBox((Rectangle){140,150,500,40},prenomInput,sizeof(prenomInput),prenomEdit);
+
+            DrawText("Email :",50,210,20,BLACK);
+            emailEdit = GuiTextBox((Rectangle){140,200,500,40},emailInput,sizeof(emailInput),emailEdit);
+
+            if(GuiButton((Rectangle){140,270,200,40},"Valider"))
+            {
+                if(strlen(nomInput)==0 || strlen(prenomInput)==0 || strlen(emailInput)==0)
+                {
+                    DrawText("Veuillez remplir tous les champs !",140,320,20,RED);
+                }
+                else
+                {
+                    ajouterUtilisateurManuel(utilisateurs,&nbUsers,nomInput,prenomInput,emailInput,0);
+                    nomInput[0]=prenomInput[0]=emailInput[0]='\0';
+                    nomEdit=prenomEdit=emailEdit=false;
+                    mode=USERS;
+                }
             }
 
-            if (GuiButton((Rectangle){140, 340, 200, 40}, "Annuler"))
+            if(GuiButton((Rectangle){140,320,200,40},"Annuler"))
             {
-                nomInput[0] = prenomInput[0] = emailInput[0] = '\0';
-                mode = USERS;
+                nomInput[0]=prenomInput[0]=emailInput[0]='\0';
+                nomEdit=prenomEdit=emailEdit=false;
+                mode=USERS;
             }
+        }
+
+        // ================== SUPPR UTILISATEUR ==================
+        else if(mode==SUPPR_USER)
+        {
+            DrawText("Supprimer un utilisateur par ID :",50,100,25,DARKGRAY);
+            int idToDel = atoi(userASupprimerBuf);
+            GuiTextBox((Rectangle){300,90,100,40},userASupprimerBuf,sizeof(userASupprimerBuf),true);
+            if(GuiButton((Rectangle){450,90,200,40},"Supprimer"))
+            {
+                supprimerUtilisateur(utilisateurs,&nbUsers,idToDel);
+                userASupprimerBuf[0]='\0';
+                mode=USERS;
+            }
+            if(GuiButton((Rectangle){600,500,250,40},"Retour")) mode=USERS;
+        }
+
+        // ================== EMPRUNTS ==================
+        else if(mode==EMPRUNTS)
+        {
+            DrawText("Gestion des emprunts",50,30,25,DARKGRAY);
+            DrawText("Cette fonctionnalité sera implémentée",50,80,20,LIGHTGRAY);
+            if(GuiButton((Rectangle){600,500,250,40},"Retour menu")) mode=MENU;
+        }
+
+        // ================== STATISTIQUES ==================
+        else if(mode==STATS)
+        {
+            DrawText("Statistiques",50,30,25,DARKGRAY);
+            DrawText("Cette fonctionnalité sera implémentée",50,80,20,LIGHTGRAY);
+            if(GuiButton((Rectangle){600,500,250,40},"Retour menu")) mode=MENU;
         }
 
         EndDrawing();
     }
 
-    // ========== Sauvegarde ==========
-    sauvegarderLivres(livres, nbLivres);
-    sauvegarderUtilisateurs(utilisateurs, nbUsers);
-    sauvegarderEmprunts(emprunts, nbEmprunts);
+    // ======= Sauvegarde des données =======
+    sauvegarderLivres(livres,nbLivres);
+    sauvegarderUtilisateurs(utilisateurs,nbUsers);
+    sauvegarderEmprunts(emprunts,nbEmprunts);
 
     CloseWindow();
     return 0;
 }
-
-
-
